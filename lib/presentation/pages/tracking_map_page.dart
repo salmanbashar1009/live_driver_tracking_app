5300c69e16a07fa183810752927cec7497195115
salmanbashar1009 <salmanbashar101@gmail.com>
2026-06-10 03:49:39 +0600

    project structure created

---
+++ lib/presentation/pages/tracking_map_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrackingMapPage extends StatelessWidget {
+  const TrackingMapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Tracking Map Page'),),
+    );
+  }
+}
