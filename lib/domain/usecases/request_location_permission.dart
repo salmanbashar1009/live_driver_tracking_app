765cb748dc790eac866c77458ed04700e22708f2
salmanbashar1009 <salmanbashar101@gmail.com>
2026-06-16 19:00:22 +0600

    feat(domain): add request location permission usecase

---
+++ lib/domain/usecases/request_location_permission.dart
@@ -0,0 +1,11 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:live_driving_tracking_app/core/errors/failures.dart';
+import 'package:live_driving_tracking_app/domain/repositories/location_repository.dart';
+
+class RequestLocationPermission {
+  final LocationRepository repository;
+  const RequestLocationPermission(this.repository);
+
+  @override
+  Future<Either<Failure,bool>> call() => repository.requestLocationPermission();
+}
